c6df3b156767ea3538bdd52c0b36f7b4394c4749
sssteiny21 <sssteiny21@gmail.com>
2016-12-05 17:53:21 -0500

    got part 2 done

--- main.js
+++ main.js
@@ -12,26 +12,33 @@ function goldController () {
   console.log("Hello!")
   
   yCtrl.goldspots = [
-    {
-      x : 484,
-      y : 334
-    },
-    {
-      x : 694,
-      y : 183
-    }
+//     {
+//       x : 484,
+//       y : 334
+//     },
+//     {
+//       x : 694,
+//       y : 183
+//     }
   ];
 
   yCtrl.position = function(clicked) {
-      console.log("Hey, this clicky thing works")
-      console.log(clicked.x, clicked.y);
-    console.log(clicked);
-      yCtrl.stickIt(clicked.x, clicked.y);
+//       console.log("Hey, this clicky thing works")
+      console.log(clicked.pageX, clicked.pageY);
+//     console.log(clicked);
+      yCtrl.stickIt(clicked.layerX, clicked.layerY);
   }
 
   yCtrl.stickIt = function(stuck_x, stuck_y) {
     console.log("Hey, Stick it works!!!")
-      yCtrl.goldspots.push({x: stuck_x, y: stuck_y});
+      yCtrl.goldspots.push({x: stuck_x, y: stuck_y - 50});
   }
-
+  
+  yCtrl.removeIt = function(event, index){
+    event.stopPropagation();
+    console.log("remove it works");
+    yCtrl.goldspots.splice(index, 1);
+    
+  }
+  
 }
